438265e45081c97ef4a63e4eeb27567034c721c7
JungIni <wd32777@gmail.com>
2020-02-01 20:09:10 +0900

    storyboard

---
+++ size/NavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationController.swift
+//  size
+//
+//  Created by dgulinc on 2020/02/01.
+//  Copyright © 2020 dgulinc. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
